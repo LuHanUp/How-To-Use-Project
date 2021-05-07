9090fe1ad0c16e1d367d45e43a0278ca973edfbd
luhan <765478939@qq.com>
2021-05-07 13:15:51 +0800

    activiti7:设置global variable方式

--- Activiti/activiti7/src/test/java/top/luhancc/use/activiti7/variable/TestSetGlobalVariable.java
+++ Activiti/activiti7/src/test/java/top/luhancc/use/activiti7/variable/TestSetGlobalVariable.java
@@ -51,4 +51,40 @@ public class TestSetGlobalVariable extends BaseTest {
         variables.put("days", 3);// 设置请假天数
         taskService.complete(task.getId(), variables);
     }
+
+    /**
+     * 通过流程实例设置variable
+     */
+    @Test
+    public void setByProcessInstance() {
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        /**
+         * executionId:流程实例的id
+         * variableName:变量名
+         * value:变量值
+         */
+//        runtimeService.setVariable("2501", "days", 3);
+        Map<String, Object> variables = new HashMap<>();
+        // 如果变量是一个pojo类型,那么需要实现序列化接口,否则会抛出异常
+        variables.put("days", 3);// 设置请假天数
+        runtimeService.setVariables("2501", variables);
+    }
+
+    @Test
+    public void setByTask() {
+        TaskService taskService = processEngine.getTaskService();
+
+        /**
+         * String taskId:任务id
+         * String variableName:变量名称
+         * Object value:变量值
+         */
+//        taskService.setVariable("1", "days", 3);
+
+        Map<String, Object> variables = new HashMap<>();
+        // 如果变量是一个pojo类型,那么需要实现序列化接口,否则会抛出异常
+        variables.put("days", 3);// 设置请假天数
+        taskService.setVariables("1", variables);
+    }
+
 }
