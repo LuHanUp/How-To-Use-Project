b11ca4862b99fc6bccd7ac68000c1a53daeb9fc3
luhan <765478939@qq.com>
2021-05-11 17:38:57 +0800

    HttpSession钝化与活化解释与使用

--- Spring/spring-mvc/src/main/java/top/luhancc/springmvc/listener/MyHttpSessionActivationListener.java
+++ Spring/spring-mvc/src/main/java/top/luhancc/springmvc/listener/MyHttpSessionActivationListener.java
@@ -5,13 +5,17 @@ import javax.servlet.http.HttpSessionEvent;
 
 /**
  * 钝化与活化的监听器HttpSessionActivationListener
- *
+ * <p>
+ * 钝化：就是对象进行序列化
+ * 活化：就是反序列化为对象
+ * <p>
+ * session钝化配置:META-INF中加入一个名为context.xml的配置文件
  * <pre>{@code
  *  <Context>
  *   <!-- maxIdleSwap:session中的对象多长时间不使用就钝化 -->
  *   <!-- directory:钝化后的对象的文件写到磁盘的哪个目录下  配置钝化的对象文件在                                                                       work/catalina/localhost/钝化文件 -->
  *   <Manager className="org.apache.catalina.session.PersistentManager"                                                                                                                      maxIdleSwap="1">
- *    <Store className="org.apache.catalina.session.FileStore" directory="igeekhome" />
+ *    <Store className="org.apache.catalina.session.FileStore" directory="" />
  *   </Manager>
  *  </Context>
  * }
@@ -24,7 +28,7 @@ import javax.servlet.http.HttpSessionEvent;
 public class MyHttpSessionActivationListener implements HttpSessionActivationListener {
 
     /**
-     * 当HttpSession中的对象被钝化时会被触发
+     * 将HttpSession序列化
      *
      * @param se
      */
@@ -34,7 +38,7 @@ public class MyHttpSessionActivationListener implements HttpSessionActivationLis
     }
 
     /**
-     * 当HttpSession的对象被活化时会被触发
+     * 反序列化HttpSession
      *
      * @param se
      */
