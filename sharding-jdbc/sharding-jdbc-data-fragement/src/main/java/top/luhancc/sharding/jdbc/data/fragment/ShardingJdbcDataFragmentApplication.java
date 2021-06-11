2d19c38a7e92e3b7cd3f2eb8e5accd49e2093018
luhan <765478939@qq.com>
2021-06-11 17:26:09 +0800

    修改sharding-jdbc-data-fragement项目的启动类名称

--- sharding-jdbc/sharding-jdbc-data-fragement/src/main/java/top/luhancc/sharding/jdbc/data/fragment/ShardingJdbcDataFragmentApplication.java
+++ sharding-jdbc/sharding-jdbc-data-fragement/src/main/java/top/luhancc/sharding/jdbc/data/fragment/ShardingJdbcDataFragmentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShardingJdbcDemoBootstrap {
+public class ShardingJdbcDataFragmentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShardingJdbcDemoBootstrap.class, args);
+        SpringApplication.run(ShardingJdbcDataFragmentApplication.class, args);
     }
 
 }
